88f04bff2edb0b32f522c16bab41a4957b9309d3
purbayankar <53092711+purbayankar@users.noreply.github.com>
2020-12-23 00:14:56 +0530

    Update dcgan.py

--- dcgan.py
+++ dcgan.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from einops import rearrange, repeat
 
 
 def weights_init(w):
@@ -55,38 +56,160 @@ class Generator(nn.Module):
         return x
 
 # Define the Discriminator Network
-class Discriminator(nn.Module):
-    def __init__(self, params):
+# class Discriminator(nn.Module):
+#     def __init__(self, params):
+#         super().__init__()
+
+#         # Input Dimension: (nc) x 64 x 64
+#         self.conv1 = nn.Conv2d(params['nc'], params['ndf'],
+#             4, 2, 1, bias=False)
+
+#         # Input Dimension: (ndf) x 32 x 32
+#         self.conv2 = nn.Conv2d(params['ndf'], params['ndf']*2,
+#             4, 2, 1, bias=False)
+#         self.bn2 = nn.BatchNorm2d(params['ndf']*2)
+
+#         # Input Dimension: (ndf*2) x 16 x 16
+#         self.conv3 = nn.Conv2d(params['ndf']*2, params['ndf']*4,
+#             4, 2, 1, bias=False)
+#         self.bn3 = nn.BatchNorm2d(params['ndf']*4)
+
+#         # Input Dimension: (ndf*4) x 8 x 8
+#         self.conv4 = nn.Conv2d(params['ndf']*4, params['ndf']*8,
+#             4, 2, 1, bias=False)
+#         self.bn4 = nn.BatchNorm2d(params['ndf']*8)
+
+#         # Input Dimension: (ndf*8) x 4 x 4
+#         self.conv5 = nn.Conv2d(params['ndf']*8, 1, 4, 1, 0, bias=False)
+
+#     def forward(self, x):
+#         x = F.leaky_relu(self.conv1(x), 0.2, True)
+#         x = F.leaky_relu(self.bn2(self.conv2(x)), 0.2, True)
+#         x = F.leaky_relu(self.bn3(self.conv3(x)), 0.2, True)
+#         x = F.leaky_relu(self.bn4(self.conv4(x)), 0.2, True)
+
+#         x = F.sigmoid(self.conv5(x))
+
+#         return x
+    
+    
+MIN_NUM_PATCHES = 16
+
+class Residual(nn.Module):
+    def __init__(self, fn):
         super().__init__()
+        self.fn = fn
+    def forward(self, x, **kwargs):
+        return self.fn(x, **kwargs) + x
 
-        # Input Dimension: (nc) x 64 x 64
-        self.conv1 = nn.Conv2d(params['nc'], params['ndf'],
-            4, 2, 1, bias=False)
+class PreNorm(nn.Module):
+    def __init__(self, dim, fn):
+        super().__init__()
+        self.norm = nn.LayerNorm(dim)
+        self.fn = fn
+    def forward(self, x, **kwargs):
+        return self.fn(self.norm(x), **kwargs)
 
-        # Input Dimension: (ndf) x 32 x 32
-        self.conv2 = nn.Conv2d(params['ndf'], params['ndf']*2,
-            4, 2, 1, bias=False)
-        self.bn2 = nn.BatchNorm2d(params['ndf']*2)
-
-        # Input Dimension: (ndf*2) x 16 x 16
-        self.conv3 = nn.Conv2d(params['ndf']*2, params['ndf']*4,
-            4, 2, 1, bias=False)
-        self.bn3 = nn.BatchNorm2d(params['ndf']*4)
-
-        # Input Dimension: (ndf*4) x 8 x 8
-        self.conv4 = nn.Conv2d(params['ndf']*4, params['ndf']*8,
-            4, 2, 1, bias=False)
-        self.bn4 = nn.BatchNorm2d(params['ndf']*8)
-
-        # Input Dimension: (ndf*8) x 4 x 4
-        self.conv5 = nn.Conv2d(params['ndf']*8, 1, 4, 1, 0, bias=False)
-
+class FeedForward(nn.Module):
+    def __init__(self, dim, hidden_dim, dropout = 0.):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(dim, hidden_dim),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(hidden_dim, dim),
+            nn.Dropout(dropout)
+        )
     def forward(self, x):
-        x = F.leaky_relu(self.conv1(x), 0.2, True)
-        x = F.leaky_relu(self.bn2(self.conv2(x)), 0.2, True)
-        x = F.leaky_relu(self.bn3(self.conv3(x)), 0.2, True)
-        x = F.leaky_relu(self.bn4(self.conv4(x)), 0.2, True)
+        return self.net(x)
 
-        x = F.sigmoid(self.conv5(x))
+class Attention(nn.Module):
+    def __init__(self, dim, heads = 8, dim_head = 64, dropout = 0.):
+        super().__init__()
+        inner_dim = dim_head *  heads
+        self.heads = heads
+        self.scale = dim ** -0.5
 
+        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias = False)
+        self.to_out = nn.Sequential(
+            nn.Linear(inner_dim, dim),
+            nn.Dropout(dropout)
+        )
+
+    def forward(self, x, mask = None):
+        b, n, _, h = *x.shape, self.heads
+        qkv = self.to_qkv(x).chunk(3, dim = -1)
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = h), qkv)
+
+        dots = torch.einsum('bhid,bhjd->bhij', q, k) * self.scale
+        mask_value = -torch.finfo(dots.dtype).max
+
+        if mask is not None:
+            mask = F.pad(mask.flatten(1), (1, 0), value = True)
+            assert mask.shape[-1] == dots.shape[-1], 'mask has incorrect dimensions'
+            mask = mask[:, None, :] * mask[:, :, None]
+            dots.masked_fill_(~mask, mask_value)
+            del mask
+
+        attn = dots.softmax(dim=-1)
+
+        out = torch.einsum('bhij,bhjd->bhid', attn, v)
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        out =  self.to_out(out)
+        return out
+
+class Transformer(nn.Module):
+    def __init__(self, dim, depth, heads, dim_head, mlp_dim, dropout):
+        super().__init__()
+        self.layers = nn.ModuleList([])
+        for _ in range(depth):
+            self.layers.append(nn.ModuleList([
+                Residual(PreNorm(dim, Attention(dim, heads = heads, dim_head = dim_head, dropout = dropout))),
+                Residual(PreNorm(dim, FeedForward(dim, mlp_dim, dropout = dropout)))
+            ]))
+    def forward(self, x, mask = None):
+        for attn, ff in self.layers:
+            x = attn(x, mask = mask)
+            x = ff(x)
         return x
+
+class Discriminator(nn.Module):
+    def __init__(self, *, image_size, patch_size, num_classes, dim, depth, heads, mlp_dim, channels = 3, dim_head = 64, dropout = 0., emb_dropout = 0.):
+        super().__init__()
+        assert image_size % patch_size == 0, 'Image dimensions must be divisible by the patch size.'
+        num_patches = (image_size // patch_size) ** 2
+        patch_dim = channels * patch_size ** 2
+        assert num_patches > MIN_NUM_PATCHES, f'your number of patches ({num_patches}) is way too small for attention to be effective (at least 16). Try decreasing your patch size'
+
+        self.patch_size = patch_size
+
+        self.pos_embedding = nn.Parameter(torch.randn(1, num_patches + 1, dim))
+        self.patch_to_embedding = nn.Linear(patch_dim, dim)
+        self.cls_token = nn.Parameter(torch.randn(1, 1, dim))
+        self.dropout = nn.Dropout(emb_dropout)
+
+        self.transformer = Transformer(dim, depth, heads, dim_head, mlp_dim, dropout)
+
+        self.to_cls_token = nn.Identity()
+
+        self.mlp_head = nn.Sequential(
+            nn.LayerNorm(dim),
+            nn.Linear(dim, num_classes)
+        )
+
+    def forward(self, img, mask = None):
+        p = self.patch_size
+
+        x = rearrange(img, 'b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1 = p, p2 = p)
+        x = self.patch_to_embedding(x)
+        b, n, _ = x.shape
+
+        cls_tokens = repeat(self.cls_token, '() n d -> b n d', b = b)
+        x = torch.cat((cls_tokens, x), dim=1)
+        x += self.pos_embedding[:, :(n + 1)]
+        x = self.dropout(x)
+
+        x = self.transformer(x, mask)
+
+        x = self.to_cls_token(x[:, 0])
+        return self.mlp_head(x)
